00d57f724a0ba5cc733cfb4d90b5e08e2b54fe63
Bilal Javed <bilal_javed95000@hotmail.com>
2021-11-18 12:37:17 +0500

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,34 +58,68 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool isLoading = true;
   final _key = UniqueKey();
+  late WebViewController _controller;
+
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+  //Make sure this function return Future<bool> otherwise you will get an error
+  Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      top: true,
-      bottom: false,
-      left: false,
-      right: false,
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            WebView(
-              key: _key,
-              initialUrl: "https://www.mubarakiyakw.com",
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageFinished: (finish) {
-                setState(() {
-                  isLoading = false;
-                });
-              },
-            ),
-            isLoading
-                // ignore: prefer_const_constructors
-                ? Center(
-                    child: const CircularProgressIndicator(),
-                  )
-                : Stack(),
-          ],
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: SafeArea(
+        top: true,
+        bottom: false,
+        left: false,
+        right: false,
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              WebView(
+                key: _key,
+                initialUrl: "https://www.mubarakiyakw.com",
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controllerCompleter.future
+                      .then((value) => _controller = value);
+                  _controllerCompleter.complete(webViewController);
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  if (request.url.contains("mailto:")) {
+                    launch(request.url);
+                    return NavigationDecision.prevent;
+                  } else if (request.url.contains("tel:")) {
+                    launch(request.url);
+                    return NavigationDecision.prevent;
+                  } else if (request.url.contains("whatsapp:")) {
+                    launch(request.url);
+                    return NavigationDecision.prevent;
+                  }
+                  return NavigationDecision.navigate;
+                },
+                onPageFinished: (finish) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+              ),
+              isLoading
+                  // ignore: prefer_const_constructors
+                  ? Center(
+                      child: const CircularProgressIndicator(),
+                    )
+                  : Stack(),
+            ],
+          ),
         ),
       ),
     );
